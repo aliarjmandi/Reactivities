b259b69a7e6d97f8e9f2b3921d328a40b9afb265
aliarjmandi <aliarjmandi@gmail.com>
2021-11-28 16:11:26 +0330

    old changes

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -8,13 +10,22 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity  Activity   {get; set;}  
             
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x=>x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _dataContext;
 
@@ -23,11 +34,14 @@ namespace Application.Activities
                 _dataContext = dataContext;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _dataContext.Activities.Add(request.Activity);
-                await _dataContext.SaveChangesAsync();
-                return Unit.Value;
+                var result  =await  _dataContext.SaveChangesAsync() >0;
+
+                if(!result) return Result<Unit>.Failure("Failed to create activity");
+             
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
